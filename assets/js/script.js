e5ef7e4fc4a3e93217248568ff85f2563a241038
SvetKZ <saryalmak@gmail.com>
2025-08-25 14:46:31 +0500

    const currencies added

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,13 @@ if ('serviceWorker' in navigator) {
     navigator.serviceWorker.register('/sconv/sw.js');
 }
 
+const kzt_won_currency = 0.380; // 1 won = kzt_won_currency KZT
+const rub_kzt_currency = 6.46;  // 1 rub = rub_kzt_currency KZT
+const usd_kzt_currency = 539;   // 1 usd = usd_kzt_currency KZT
+
+const rub_won_k = kzt_won_currency / rub_kzt_currency;
+const usd_kzt_k = kzt_won_currency / usd_kzt_currency;
+
 const tableBtn = document.getElementById('table-btn');
 const input = document.getElementById('input');
 const quantityInput = document.getElementById('quantity');
@@ -31,9 +38,9 @@ function calculate()
     const won = parseFloat(input.value);
     const quantity = parseFloat(quantityInput.value);
 
-    const kzt = won / 1000 * 380;
-    const rub = won / 1000 * 59;
-    const usd = won / 10000 * 7;
+    const kzt = won * kzt_won_currency;
+    const rub = kzt * rub_won_k;
+    const usd = kzt * usd_kzt_k;
 
     const totalKzt = kzt * quantity;
     const totalRub= rub * quantity;
