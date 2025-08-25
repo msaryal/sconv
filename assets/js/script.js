d22d7089519f37651ac99da0bdfbefcc34c784a4
SvetKZ <saryalmak@gmail.com>
2025-08-25 15:03:09 +0500

    add currencies inputs

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,9 +2,9 @@ if ('serviceWorker' in navigator) {
     navigator.serviceWorker.register('/sconv/sw.js');
 }
 
-const kzt_won_currency = 0.380; // 1 won = kzt_won_currency KZT
-const rub_kzt_currency = 6.46;  // 1 rub = rub_kzt_currency KZT
-const usd_kzt_currency = 539;   // 1 usd = usd_kzt_currency KZT
+let won_kzt_currency = 0.380; // 1 won = kzt_won_currency KZT
+let rub_kzt_currency = 6.46;  // 1 rub = rub_kzt_currency KZT
+let usd_kzt_currency = 539;   // 1 usd = usd_kzt_currency KZT
 
 const tableBtn = document.getElementById('table-btn');
 const input = document.getElementById('input');
@@ -32,10 +32,28 @@ function calculate()
         return;
     }
 
+    const won_kzt_currency_input = +document.getElementById('won_kzt_currency').value;
+    const rub_kzt_currency_input = +document.getElementById('rub_kzt_currency').value;
+    const usd_kzt_currency_input = +document.getElementById('usd_kzt_currency').value;
+
+    console.log(won_kzt_currency_input, rub_kzt_currency_input, usd_kzt_currency_input);
+
+    if(won_kzt_currency_input) {
+        won_kzt_currency = won_kzt_currency_input;
+    }
+
+    if(rub_kzt_currency_input) {
+        rub_kzt_currency = rub_kzt_currency_input;
+    }
+
+    if(usd_kzt_currency_input) {
+        usd_kzt_currency = usd_kzt_currency_input;
+    }
+
     const won = parseFloat(input.value);
     const quantity = parseFloat(quantityInput.value);
 
-    const kzt = won * kzt_won_currency;
+    const kzt = won * won_kzt_currency;
     const rub = Math.round(kzt / rub_kzt_currency * 100) / 100;
     const usd = Math.round(kzt / usd_kzt_currency * 100) / 100;
 
